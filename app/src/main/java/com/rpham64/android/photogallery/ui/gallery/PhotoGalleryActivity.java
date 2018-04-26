04f888c48b99c0c82697e23f8f252a245517db65
rpham64 <rpham64@gmail.com>
2018-04-25 23:41:39 -0700

    Changed PhotoGalleryActivity to contain View and Presenter (correct implementation of MVP)

--- app/src/main/java/com/rpham64/android/photogallery/ui/gallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/rpham64/android/photogallery/ui/gallery/PhotoGalleryActivity.java
@@ -1,18 +1,38 @@
 package com.rpham64.android.photogallery.ui.gallery;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-import com.rpham64.android.photogallery.ui.SingleFragmentActivity;
+import com.rpham64.android.photogallery.R;
+import com.rpham64.android.photogallery.utils.ActivityUtils;
+import com.rpham64.android.photogallery.utils.SearchQuerySharedPreference;
 
 /**
- * Main Activity of the "Gallery" portion of this app.
+ * Main Activity that acts as the container for holding the View ({@link PhotoGalleryFragment}) and
+ * Presenter ({@link PhotoGalleryPresenter}) components of the Gallery.
  */
-public class PhotoGalleryActivity extends SingleFragmentActivity {
+public class PhotoGalleryActivity extends AppCompatActivity {
 
     private static final String TAG = PhotoGalleryActivity.class.getSimpleName();
 
+    private PhotoGalleryPresenter mPhotoGalleryPresenter;
+
     @Override
-    protected Fragment createFragment() {
-        return PhotoGalleryFragment.newInstance();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        PhotoGalleryFragment photoGalleryFragment =
+                (PhotoGalleryFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+
+        if (photoGalleryFragment == null) {
+            photoGalleryFragment = PhotoGalleryFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
+                    photoGalleryFragment, R.id.fragment_container);
+        }
+
+        SearchQuerySharedPreference searchQuerySharedPreference = new SearchQuerySharedPreference(this);
+        mPhotoGalleryPresenter = new PhotoGalleryPresenter(photoGalleryFragment, searchQuerySharedPreference);
     }
 }
